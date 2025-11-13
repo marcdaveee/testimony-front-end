ba6e64ea996df42032e2cf5240db437d77b5b501
SITP_USER1 <davevalencia17@gmail.com>
2025-11-13 17:04:21 +0900

    feat (utils) - create utility function to extract error returned by API and grouped them accordingly - set the corresponding errors to their assigned input field name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,44 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function extractErrors(
+  errors: string[],
+  fieldName: string,
+  stripFirstWord?: boolean
+) {
+  const relatedFieldErrors = errors.filter(item =>
+    item.split(" ")[0].toLowerCase().includes(fieldName)
+  );
+
+  if (relatedFieldErrors.length > 0) {
+    // if there's a related field error
+    let formattedErrMessages: string[] = [];
+
+    relatedFieldErrors.forEach(relatedErr => {
+      if (relatedErr.split(" ")[0].toLowerCase().includes(fieldName)) {
+        let msg = "";
+        relatedErr.split(" ").forEach((str, index) => {
+          if (stripFirstWord) {
+            if (index != 0) {
+              msg = msg + str + " ";
+            }
+          } else {
+            msg = msg + str + " ";
+          }
+        });
+        msg = msg.trimEnd();
+        msg = msg[0].toUpperCase() + msg.slice(1);
+
+        formattedErrMessages.push(msg);
+      }
+    });
+
+    return formattedErrMessages;
+  } else {
+    return undefined;
+  }
 }
